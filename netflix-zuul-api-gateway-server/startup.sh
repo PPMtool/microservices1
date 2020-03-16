6c148902cdabc6bbfd8a6de31a8771b739fe726b
sivakiran9 <47302042+sivakiran9@users.noreply.github.com>
2020-03-16 19:39:30 +0530

    Update startup.sh

--- netflix-zuul-api-gateway-server/startup.sh
+++ netflix-zuul-api-gateway-server/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-nohup java -jar -Xmx268m /app/netflix-zuul-api-gateway-server-0.0.1-SNAPSHOT.jar &
+nohup java -jar -Xmx268m /app/netflix-zuul-api-gateway-server-0.0.1-SNAPSHOT.jar >/dev/null 2>&1&
+
